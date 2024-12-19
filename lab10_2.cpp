ee4b06cf52baa76b5d973dec6cca386ba11b3fe7
wasawatkp <wasawat.upper@gmail.com>
2024-12-19 23:52:18 +0700

    lab10_1 Clear + cheer book copy

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,11 +6,19 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	string Text;
+	
+	cout << "-------------------- BOOM ---------------------\n";
+	while(getline(source , Text)){
+	    cout << Text << "\n" ;
+	    
+	}
+	cout << "-------------------- HA!! ---------------------\n";
 
     source.close();
     dest.close();
